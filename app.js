68df58848cb0f64b29615decfa6d0f699eb82af4
Gary Tapia <gary.tapia2009@gmail.com>
2021-02-11 14:32:45 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Beanstalk-Example!'));
+app.get('/', (req, res) => res.send('Full Pipeline Demo!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
